756baa50f303712802aeb2a7365944fbc5f6f6c0
HanGyulKang <gang6607@gmail.com>
2022-12-21 00:20:23 +0900

    first commit

---
+++ src/test/java/com/study/ex1jpabasic/Ex1JpaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ex1jpabasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex1JpaBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
